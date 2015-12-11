5c01dbc73137e22060473ae3ef5bb00995613af8
Muhammad Awais <Muhammad Awais>
2015-12-11 02:08:51 -0800

    second commit

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 var express = require('express');
-var fs      = require('fs');
 var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 app.get('/',function(req,res){
-    res.send('Chat Application Coming Soon');
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, ipaddress, function() {
+io.on('connection', function(socket){
+	socket.on('disconnect',function(){
+		console.log('a client disconnected');
+	});
+
+	socket.on('chat message', function(msg){
+    	//console.log('message: ' + msg);
+    	io.emit('chat message', msg);
+  	});
+  	console.log('a client connected');
+});
+
+http.listen(port, ipaddress, function() {
     console.log('%s: Node server started on %s:%d ...',
                 Date(Date.now() ), ipaddress, port);
 });
